7539dcfe1a6d562431aa1e17a82f6ab526588f10
owenfun07 <owen.vozzella@gmail.com>
2026-03-10 17:37:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ Never robotic.
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
